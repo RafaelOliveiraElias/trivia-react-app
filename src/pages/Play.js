557ab6827d3776c332a2c4db4674f8f9acb5baad
Igor Leal <igorhenriqueleal3@gmail.com>
2022-04-05 22:53:26 -0300

    Requisito 5 iniciado

--- src/pages/Play.js
+++ src/pages/Play.js
@@ -1,11 +1,41 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import { saveLogin } from '../redux/actions';
+import { saveLogin, fetchQuestion, fetchToken } from '../redux/actions';
 
 class Play extends React.Component {
+  componentDidMount() {
+    const { dispatchQuestion, token, question, getToken } = this.props;
+    dispatchQuestion(token);
+    const number = 3;
+    if (question.response_code === number) {
+      getToken();
+      dispatchQuestion(token);
+    }
+  }
+
+  // Referência: https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+  shuffleArray = (array) => {
+    const result = array
+      .map((value) => ({ value, sort: Math.random() }))
+      .sort((a, b) => a.sort - b.sort)
+      .map(({ value }) => value);
+
+    return result;
+  }
+
+  arraySort = () => {
+    const { question } = this.props;
+    const incorrectAnswer = question.results[0].incorrect_answers.map((element, index) => ({
+      value: element,
+      index,
+    }));
+    const result = [{ value: question.results[0].correct_answer }, ...incorrectAnswer];
+    return this.shuffleArray(result);
+  }
+
   render() {
-    const { player } = this.props;
+    const { player, question } = this.props;
     const { gravatarEmail, name, score } = player;
     return (
       <div>
@@ -14,7 +44,43 @@ class Play extends React.Component {
           <p data-testid="header-player-name">{ name }</p>
           <p data-testid="header-score">{ score }</p>
         </header>
-        <h1>Play</h1>
+        <main>
+          <p
+            data-testid="question-category"
+          >
+            {
+              Object.keys(question).length !== 0
+                ? question.results[0].category : 'Loading...'
+            }
+          </p>
+          <p
+            data-testid="question-text"
+          >
+            {
+              Object.keys(question).length !== 0
+                ? question.results[0].question : 'Loading...'
+            }
+          </p>
+          <div data-testid="answer-options">
+            {
+              Object.keys(question).length !== 0
+                ? this.arraySort().map((element, index) => (
+                  <button
+                    type="button"
+                    key={ index }
+                    data-testid={
+                      element.index
+                        ? `wrong-answer-${index}`
+                        : 'correct_answer'
+                    }
+                  >
+                    { element.value }
+                  </button>
+                ))
+                : null
+            }
+          </div>
+        </main>
       </div>
     );
   }
@@ -22,11 +88,15 @@ class Play extends React.Component {
 
 const mapDispatchToProps = (dispatch) => ({
   login: (e) => dispatch(saveLogin(e)),
+  dispatchQuestion: (token) => dispatch(fetchQuestion(token)),
+  getToken: () => dispatch(fetchToken()),
 });
 
 const mapStateToProps = (state) => ({
   token: state.token,
   player: state.player,
+  question: state.question,
+  isFetchingQuestion: state.isFetchingQuestion,
 });
 
 Play.propTypes = ({
--- src/redux/reducers/token.js
+++ src/redux/reducers/token.js
@@ -1,16 +1,27 @@
-import { RECEIVE_TOKEN_FAILURE, RECEIVE_TOKEN_SUCCESS, REQUEST_TOKEN } from '../actions';
+import {
+  RECEIVE_TOKEN_FAILURE, RECEIVE_TOKEN_SUCCESS, REQUEST_TOKEN,
+  RECEIVE_QUESTION_SUCCESS, RECEIVE_QUESTION_FAILURE, REQUEST_QUESTION,
+} from '../actions';
 
 const INITIAL_STATE = {
   token: '',
+  question: {},
   player: { name: '',
     email: '',
     score: 0,
     gravatarEmail: '',
   },
+  isFetchingQuestion: false,
+  isFetching: false,
 };
 
 function token(state = INITIAL_STATE, action) {
   switch (action.type) {
+  case REQUEST_QUESTION:
+    return {
+      ...state,
+      isFetchingQuestion: true,
+    };
   case REQUEST_TOKEN:
     return {
       ...state,
@@ -31,11 +42,17 @@ function token(state = INITIAL_STATE, action) {
       isFetching: false,
       token: action.token,
     };
-  case RECEIVE_TOKEN_FAILURE:
+  case RECEIVE_TOKEN_FAILURE || RECEIVE_QUESTION_FAILURE:
     return {
       ...state,
       isFetching: false,
       error: action.error,
+    };
+  case RECEIVE_QUESTION_SUCCESS:
+    return {
+      ...state,
+      isFetchingQuestion: false,
+      question: action.question,
     };
   default:
     return state;
