7fe1899112492592fd89df7cd6a9a63ad9550645
phvleite <phvleite@gmail.com>
2022-04-06 16:32:50 -0300

    requisito 12 e 13 prontos mas não testados

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import Login from './pages/Login';
 import Play from './pages/Play';
 import Settings from './pages/Settings';
+import Feedback from './pages/Feedback';
 
 class App extends React.Component {
   render() {
@@ -12,6 +13,7 @@ class App extends React.Component {
         <Route exact path="/" component={ Login } />
         <Route exact path="/play" component={ Play } />
         <Route exact path="/settings" component={ Settings } />
+        <Route exact path="/feedback" component={ Feedback } />
       </Switch>
     );
   }
