ec4aa39b830c60da2d0ae27199f594dfc539ea35
bruno <marqsbruno@gmail.com>
2022-04-07 19:17:26 -0300

    req 17

--- src/pages/Ranking.js
+++ src/pages/Ranking.js
@@ -1,11 +1,32 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Redirect } from 'react-router-dom';
 
 class Ranking extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      redirect: false,
+    };
+  }
+
   render() {
     console.log(this.props);
+    const { redirect } = this.state;
     return (
-      <h1 data-testid="ranking-title">Settings</h1>
+      <>
+        <h1 data-testid="ranking-title">Ranking</h1>
+        <section>
+          <button
+            type="button"
+            data-testid="btn-go-home"
+            onClick={ () => this.setState({ redirect: true }) }
+          >
+            Play Again
+          </button>
+          { redirect && <Redirect to="/" />}
+        </section>
+      </>
     );
   }
 }
