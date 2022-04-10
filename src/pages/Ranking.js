152a1821a467b11652e7a9f47c3ffbb851148227
phvleite <phvleite@gmail.com>
2022-04-10 15:13:32 -0300

    css pagina ranking finalizado

--- src/pages/Ranking.js
+++ src/pages/Ranking.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Redirect } from 'react-router-dom';
+import WhoWant from '../images/who-want-to-be-a-millionaire.png';
+import GoldMedal from '../images/goldMedal.png';
+import '../css/Ranking.css';
 
 class Ranking extends React.Component {
   constructor(props) {
@@ -23,26 +26,46 @@ class Ranking extends React.Component {
     const { redirect, ranking } = this.state;
     return (
       <>
-        <h1 data-testid="ranking-title">Ranking</h1>
-        <section>
-          {ranking.map((each, index) => (
-            <div key={ index }>
-              <h3>{`${index + 1}.`}</h3>
-              <img src={ `https://www.gravatar.com/avatar/${each.picture}` } alt={ each.name } />
-              <h3 data-testid={ `player-name-${index}` }>{each.name}</h3>
-              <h3 data-testid={ `player-score-${index}` }>{each.score}</h3>
+        <header>
+          <div className="box-header-ranking">
+            <div>
+              <img
+                src={ GoldMedal }
+                alt="imagem medalha de ouro"
+                className="gold-medal-img"
+              />
             </div>
-          ))}
-        </section>
-        <section>
-          <button
-            type="button"
-            data-testid="btn-go-home"
-            onClick={ () => this.setState({ redirect: true }) }
-          >
-            Play Again
-          </button>
-          { redirect && <Redirect to="/" />}
+            <div className="box-header-title">
+              <img src={ WhoWant } alt="logo do jogo" className="who-want-img" />
+              <h1 data-testid="ranking-title">Ranking</h1>
+            </div>
+          </div>
+        </header>
+        <section className="box-main-ranking">
+          <section className="box-list-ranking">
+            {ranking.map((each, index) => (
+              <div key={ index } className="box-each-ranking">
+                <div className="box-position-ranking">
+                  <h3>{`${index + 1}.`}</h3>
+                  <img src={ `https://www.gravatar.com/avatar/${each.picture}` } alt={ each.name } />
+                </div>
+                <div className="box-details-ranking">
+                  <h3 data-testid={ `player-name-${index}` }>{each.name}</h3>
+                  <h3 data-testid={ `player-score-${index}` }>{each.score}</h3>
+                </div>
+              </div>
+            ))}
+          </section>
+          <section className="box-button-ranking">
+            <button
+              type="button"
+              data-testid="btn-go-home"
+              onClick={ () => this.setState({ redirect: true }) }
+            >
+              Play Again
+            </button>
+            { redirect && <Redirect to="/" />}
+          </section>
         </section>
       </>
     );
